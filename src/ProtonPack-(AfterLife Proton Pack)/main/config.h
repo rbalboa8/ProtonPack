a5d09cd60ba03a9eca293d91e79164bfb5d981b5
rbalboa8 <r_balboa8@hotmail.com>
2026-01-06 15:23:37 +0000

    Bargraph library re-upload

--- src/ProtonPack-(AfterLife Proton Pack)/main/config.h
+++ src/ProtonPack-(AfterLife Proton Pack)/main/config.h
@@ -7,5 +7,5 @@
 // --- Bar Graph Hardware Type ---
 // Please choose the line that matches your hardware.
 
-const bargraph_hardware_t BARGRAPH_HARDWARE = BARGRAPH_COMMON_ANODE;
+//const bargraph_hardware_t BARGRAPH_HARDWARE = BARGRAPH_COMMON_ANODE;
 //const bargraph_hardware_t BARGRAPH_HARDWARE = BARGRAPH_COMMON_CATHODE;
