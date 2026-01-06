62e6b0bf413073c248467e29db9c94443cc75825
rbalboa8 <r_balboa8@hotmail.com>
2026-01-06 15:34:45 +0000

    Bin file

--- src/ProtonPack-(AfterLife Proton Pack)/main/config.h
+++ src/ProtonPack-(AfterLife Proton Pack)/main/config.h
@@ -8,4 +8,4 @@
 // Please choose the line that matches your hardware.
 
 //const bargraph_hardware_t BARGRAPH_HARDWARE = BARGRAPH_COMMON_ANODE;
-//const bargraph_hardware_t BARGRAPH_HARDWARE = BARGRAPH_COMMON_CATHODE;
+const bargraph_hardware_t BARGRAPH_HARDWARE = BARGRAPH_COMMON_CATHODE;
